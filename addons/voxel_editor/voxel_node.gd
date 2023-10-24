45a21284c8257d76807321c7e91b0a8e79dd2358
piwicode <pierre.labatut@gmail.com>
2023-10-24 09:10:00 +0200

    Move static data construction in a static initialization.

--- addons/voxel_editor/voxel_node.gd
+++ addons/voxel_editor/voxel_node.gd
@@ -15,7 +15,7 @@ var mesh_index_map: Dictionary = build_mesh_index_map()
 var material_map: Dictionary
 
 const BOX_AXIS_VALUES = [-0.5, 0.5]
-const MESH_BF_SEEDS = [0x17, 0x5f, 0x7f, 0xff]
+const MESH_ID_SEEDS = [0x17, 0x5f, 0x7f, 0xff]
 const ORTHO_BASES = [
 	Basis(Vector3(1, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1)),
 	Basis(Vector3(0, -1, 0), Vector3(1, 0, 0), Vector3(0, 0, 1)),
@@ -61,7 +61,7 @@ func get_material_for(color: Color) -> StandardMaterial3D:
 	return material_map[color]
 
 
-static func transform_bf(mesh_id: int, basis: Basis) -> int:
+static func transform_id(mesh_id: int, basis: Basis) -> int:
 	var result = 0
 	var p2 = Vector3(1, 2, 4)  # Powers of two.
 	for z in BOX_AXIS_VALUES:
@@ -78,11 +78,11 @@ static func build_mesh_index_map() -> Dictionary:
 	var start_time = Time.get_ticks_msec()
 	var mapping = {}
 
-	for mesh_id in MESH_BF_SEEDS:
+	for mesh_id in MESH_ID_SEEDS:
 		var scene = load("res://addons/voxel_editor/res/mesh_%d.tscn" % mesh_id)
 		var mesh = load("res://addons/voxel_editor/res/mesh_%d.res" % mesh_id)
 		for basis in ORTHO_BASES:
-			var rotated_mesh_id = transform_bf(mesh_id, basis)
+			var rotated_mesh_id = transform_id(mesh_id, basis)
 			if not rotated_mesh_id in mapping:
 				mapping[rotated_mesh_id] = {scene = scene, mesh = mesh, basis = basis}
 
@@ -159,7 +159,17 @@ func _on_visibility_changed():
 		__clear_cell_children()
 
 
-static func make_face(mask: int, bf: int, vertices: int, normal: Vector3, outer: bool):
+class Face:
+	var mask: int
+	var id: int
+	var vertices: PackedVector3Array
+	var normal: Vector3
+	var neighbour: Vector3i
+	var neighbour_mask: int
+	var neighbour_value: int
+
+
+static func __make_face(mask: int, id: int, vertices: int, normal: Vector3, outer: bool) -> Face:
 	var vertex_array = PackedVector3Array()
 	var bits = vertices
 	for z in BOX_AXIS_VALUES:
@@ -178,75 +188,78 @@ static func make_face(mask: int, bf: int, vertices: int, normal: Vector3, outer:
 	if (vertex_array[0] - vertex_array[1]).cross(vertex_array[1] - vertex_array[2]).dot(normal) > 0:
 		vertex_array.reverse()
 	var n_i = Vector3i(normal)
-	return {
-		mask = mask,
-		bf = bf,
-		vertices = vertex_array,
-		normal = normal.normalized(),
-		neighbour = n_i if outer else null,
-		neighbour_mask =
-		bf << max(0, FaceShift[-n_i]) >> max(0, -FaceShift[-n_i]) if outer else null,
-		neighbour_value =
-		bf << max(0, FaceShift[-n_i]) >> max(0, -FaceShift[-n_i]) if outer else null
-	}
+	var face: Face = Face.new()
+
+	face.mask = mask
+	face.id = id
+	face.vertices = vertex_array
+	face.normal = normal.normalized()
+	face.neighbour = n_i if outer else Vector3i.ZERO
+	face.neighbour_mask = id << max(0, FaceShift[-n_i]) >> max(0, -FaceShift[-n_i]) if outer else 0
+	face.neighbour_value = id << max(0, FaceShift[-n_i]) >> max(0, -FaceShift[-n_i]) if outer else 0
+	return face
+
+
+static func __build_faces_by_mesh_id() -> Dictionary:
+	print("Generate mesh export primitives.")
+	var face_seeds = [
+		__make_face(0x0f, 0x0f, 0x0f, Vector3(0, 0, -1), true),  # outer square.
+		__make_face(0x0f, 0x07, 0x07, Vector3(0, 0, -1), true),  # outer triangle.
+		__make_face(0xff, 0x5f, 0x5a, Vector3(1, 0, 1), false),  # inner square.
+		__make_face(0xff, 0x17, 0x16, Vector3(1, 1, 1), false),  # inner triangle.
+		__make_face(0xff, 0x7f, 0x68, Vector3(1, 1, 1), false),  # inner triangle.
+	]
+
+	var face_by_id = {}
+	for seed in face_seeds:
+		for basis in ORTHO_BASES:
+			var id = transform_id(seed.id, basis)
+			if id in face_by_id:
+				continue
+			var face: Face = Face.new()
+
+			face.mask = transform_id(seed.mask, basis)
+			face.id = id
+			# TODO: use the basis once Basis implement * operator for PackedVector3Array
+			face.vertices = Transform3D(basis, Vector3.ZERO) * seed.vertices
+			face.normal = basis * seed.normal
+			face.neighbour = Vector3i(basis * seed.normal) if seed.neighbour else Vector3i.ZERO
+			face.neighbour_mask = transform_id(seed.neighbour_mask, basis) if seed.neighbour else 0
+			face.neighbour_value = (
+				transform_id(seed.neighbour_value, basis) if seed.neighbour else 0
+			)
+			face_by_id[id] = face
+
+	var faces_by_mesh_id: Dictionary = {}
+	for mesh_id_seeds in MESH_ID_SEEDS:
+		for basis in ORTHO_BASES:
+			var mesh_id: int = transform_id(mesh_id_seeds, basis)
+			if mesh_id in faces_by_mesh_id:
+				continue
+			var faces: Array[Face] = []
+			for face in face_by_id.values():
+				if mesh_id & face.mask == face.id:
+					faces.append(face)
+			faces_by_mesh_id[mesh_id] = faces
+
+	return faces_by_mesh_id
+
+
+static var FACES_BY_MESH_ID = __build_faces_by_mesh_id()
+
+static var UV_LST: Dictionary = {
+	3: PackedVector2Array([Vector2(1, 0), Vector2(1, -1), Vector2(0, 0)]),
+	6:
+	PackedVector2Array(
+		[Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(1, 1), Vector2(0, 1), Vector2(0, 0)]
+	)
+}
 
 
 func export_mesh():
 	print("Export mesh")
+	var faces_by_mesh_id: Dictionary = FACES_BY_MESH_ID
 	var time_start = Time.get_ticks_usec()
-	var face_seeds = [
-		make_face(0x0f, 0x0f, 0x0f, Vector3(0, 0, -1), true),  # outer square.
-		make_face(0x0f, 0x07, 0x07, Vector3(0, 0, -1), true),  # outer triangle.
-		make_face(0xff, 0x5f, 0x5a, Vector3(1, 0, 1), false),  # inner square.
-		make_face(0xff, 0x17, 0x16, Vector3(1, 1, 1), false),  # inner triangle.
-		make_face(0xff, 0x7f, 0x68, Vector3(1, 1, 1), false),  # inner triangle.
-	]
-
-	var face_by_bf = {}
-	for face in face_seeds:
-		for basis in ORTHO_BASES:
-			var bf = transform_bf(face.bf, basis)
-			if bf in face_by_bf:
-				continue
-			face_by_bf[bf] = {
-				mask = transform_bf(face.mask, basis),
-				bf = bf,
-				# TODO: use the basis once Basis implement * operator for PackedVector3Array.
-				vertices = Transform3D(basis, Vector3.ZERO) * face.vertices,
-				normal = basis * face.normal,
-				neighbour = Vector3i(basis * face.normal) if face.neighbour else null,
-				neighbour_mask =
-				transform_bf(face.neighbour_mask, basis) if face.neighbour else null,
-				neighbour_value =
-				transform_bf(face.neighbour_value, basis) if face.neighbour else null,
-			}
-	var faces_by_mesh_bf = {}
-	for mesh_bf_seeds in MESH_BF_SEEDS:
-		for basis in ORTHO_BASES:
-			var mesh_bf = transform_bf(mesh_bf_seeds, basis)
-			if mesh_bf in faces_by_mesh_bf:
-				continue
-			var faces = []
-			for face in face_by_bf.values():
-				if mesh_bf & face.mask == face.bf:
-					faces.append(face)
-			faces_by_mesh_bf[mesh_bf] = faces
-
-	var UV_LST = {
-		3: PackedVector2Array([Vector2(1, 0), Vector2(1, -1), Vector2(0, 0)]),
-		6:
-		PackedVector2Array(
-			[
-				Vector2(0, 0),
-				Vector2(1, 0),
-				Vector2(1, 1),
-				Vector2(1, 1),
-				Vector2(0, 1),
-				Vector2(0, 0)
-			]
-		)
-	}
-	var time_const_done = Time.get_ticks_usec()
 	var vertices = PackedVector3Array()
 	var normals = PackedVector3Array()
 	var colors = PackedColorArray()
@@ -254,7 +267,8 @@ func export_mesh():
 	var skipped = 0
 	for coord in map:
 		var cell = map[coord]
-		for face in faces_by_mesh_bf[cell.mesh_id]:
+		for face_v in faces_by_mesh_id[cell.mesh_id]:
+			var face: Face = face_v
 			if face.neighbour:
 				if get_cell(coord + face.neighbour) & face.neighbour_mask == face.neighbour_value:
 					skipped += face.vertices.size()
@@ -285,8 +299,7 @@ func export_mesh():
 	var time_mesh_done = Time.get_ticks_usec()
 	ResourceSaver.save(arr_mesh, "res://export_test.tres", ResourceSaver.FLAG_COMPRESS)
 	print("Done ", vertices.size(), " vertices (", skipped, " skipped)")
-	print(" - const ", (time_const_done - time_start) / 1000., " ms")
-	print(" - geom ", (time_geom_done - time_const_done) / 1000., " ms")
+	print(" - geom ", (time_geom_done - time_start) / 1000., " ms")
 	print(" - mesh ", (time_mesh_done - time_geom_done) / 1000., " ms")
 	print(" - all ", (time_mesh_done - time_start) / 1000., " ms")
 
