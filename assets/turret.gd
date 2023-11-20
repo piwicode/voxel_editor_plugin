726a3d5395b63af0c740177fa6910c984d46a6b3
piwicode <pierre.labatut@gmail.com>
2023-11-20 22:06:18 +0100

    Tune automation

--- assets/turret.gd
+++ assets/turret.gd
@@ -2,16 +2,23 @@ extends Node3D
 var global_target : Vector3
 
 var body_controler: PIDControler = PIDControler.new(1500, 0, 250)
-var cannon_controler: PIDControler = PIDControler.new(1500, 100, 300)
+var cannon_controler: PIDControler = PIDControler.new(1500, 0, 500)
 
-var fire_cooldown_sec: float = .2
-var burst_count: float = 5
-var burst_cooldown_sec: float = 2
+const fire_cooldown_sec: float = .2
+const burst_count: float = 5
+const burst_cooldown_sec: float = 2
 
 var burst_current_countdown: int = burst_count
 var fire_current_cooldown: float = 0 
 
+var cannon_lower: float
+var cannon_upper: float
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	cannon_lower = -%cannon_joint["angular_limit/upper"] 
+	cannon_upper = -%cannon_joint["angular_limit/lower"]
+	
 func process_fire(delta):
 	fire_current_cooldown -= delta
 
@@ -33,7 +40,8 @@ class PIDControler:
 	var previous_error: float = INF
 	var error_integral: float = 0
 	var pid: Vector3
-
+	var log: bool = false
+	var log_countdown : int = 0
 	func _init(p:float, i:float, d:float):
 		self.pid = Vector3(p,i,d)
 
@@ -41,8 +49,17 @@ class PIDControler:
 		if previous_error == INF:
 			previous_error = error
 		error_integral += error * delta
-		var error_derivate = (error - previous_error ) / delta
+		var error_derivate = (error - previous_error) / delta
 		previous_error = error
+		if log:
+			log_countdown -= 1
+			if log_countdown <= 0:
+				log_countdown = 8
+				print("error: %f -> %f   int: %f -> %f   der: %f -> %f  c: %f" % [
+				error, error * pid.x, 
+				error_integral, error_integral * pid.y,
+				error_derivate, error_derivate * pid.z,
+				pid.dot(Vector3(error, error_integral, error_derivate))])
 		return pid.dot(Vector3(error, error_integral, error_derivate))
 
 
@@ -55,16 +72,20 @@ func process_automation(delta):
 	var body_control = body_controler.process(body_error, delta)
 	%body.apply_torque_impulse(Vector3(0, body_control * delta, 0))
 	
+	# Pilot the cannon rotation.
 	var target_angle = atan2(body_local.y, sqrt(body_local.x *body_local.x + body_local.z*body_local.z))
+	# Do not target something out of range, otherwise the integral grows out of
+	# control.
+	target_angle = clamp(target_angle, cannon_lower, cannon_upper)
 	var cannon_error = (target_angle - %cannon.rotation.z)
+
+#	cannon_controler.pid = Vector3(1500, 0, 500)
+#	cannon_controler.log = true
+	var i = cannon_controler.error_integral
 	var cannon_control = cannon_controler.process(cannon_error, delta)
-#	%cannon.apply_torque_impulse(
-#		%cannon.global_transform.basis * Vector3(0, 0, cannon_control * delta))
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+	
+	%cannon.apply_torque_impulse(
+		%cannon.global_transform.basis * Vector3(0, 0, cannon_control * delta))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- game.gd
+++ game.gd
@@ -5,13 +5,18 @@ var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	%turret_asm.global_target = Vector3(15, 10, -10)
-
+	%turret_asm.global_target = Vector3(30, 30, 0)
+	%target.position = %turret_asm.global_target
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		%turret_asm.global_target = Vector3(
-			rng.randi_range(20, 40), rng.randi_range(-20, 20), rng.randi_range(-20, 20)
+			rng.randi_range(20, 40), 
+			rng.randi_range(-20, 20),
+			rng.randi_range(-20, 20)
 		)
 		%target.position = %turret_asm.global_target
+
+# 1.0473541021347
+#-0.12435601651669
